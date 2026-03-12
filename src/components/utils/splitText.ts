f341fee2a0938715fb7888dc8e2dda0d2cd0de20
Vishal Bangarga <vk3606169@gmail.com>
2026-03-12 12:33:46 +0530

    bug fixed

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -26,13 +26,14 @@ export default function setSplitText() {
       para.split?.revert();
     }
 
-    para.split = new SplitText(para, {
+    const split = new SplitText(para, {
       type: "lines,words",
       linesClass: "split-line",
     });
+    para.split = split;
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      split.words,
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -53,12 +54,14 @@ export default function setSplitText() {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
-    title.split = new SplitText(title, {
+    const split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+    title.split = split;
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      split.chars,
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
